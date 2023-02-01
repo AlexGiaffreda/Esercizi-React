161136a0c3480613b76aed4306e9e312b60b1152
Alex <alex.giaffreda7@gmail.com>
2023-02-01 16:39:00 +0100

    esercizio_1_JSX

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const hello = <h1>Hello, World!</h1>
+  return (
+    <div className="App">
+      {hello}
+    </div>
+  );
+}
+
+export default App;
