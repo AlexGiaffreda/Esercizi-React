8b78183a9e9f91ecff7f8a7aac95b9239dcd0ec7
Alex <alex.giaffreda7@gmail.com>
2023-02-04 19:43:59 +0100

    events 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { ClickCounter } from './ClickCounter';
 import { Counter } from './Counter';
 
 function App() {
   return (
     <div className="App">
       <Counter interval = {1000} increment = {1} starting = {0}/>
+      <ClickCounter />
     </div>
   );
 }
