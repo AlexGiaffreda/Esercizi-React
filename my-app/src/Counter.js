57438a5e4dca0718f69d14e626536c874abec151
Alex <alex.giaffreda7@gmail.com>
2023-02-04 19:19:33 +0100

    state 4

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -4,11 +4,12 @@ import { CounterDisplay } from './CounterDisplay';
 export class Counter extends React.Component {
     state = { count : this.props.starting }
 
-    componentDidMount(){
+    constructor(props) {
+        super(props)
 
         setInterval(() => {
-            this.setState((state) => {
-                return {count: this.state.count + this.props.increment}
+            this.setState({
+                count: this.state.count + this.props.increment
             })
 
         }, this.props.interval)
