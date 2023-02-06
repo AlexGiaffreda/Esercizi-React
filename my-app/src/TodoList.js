688b791b05be2422ce97ec68915994e434172b4d
Alex <alex.giaffreda7@gmail.com>
2023-02-06 17:26:43 +0100

    lists 6

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -11,31 +11,58 @@ export default  class Todolist extends React.Component {
 
     eventHandler = (event) => {
         this.setState((state) => {
-            return { addItems: state.addItems = event.target.value }
+            return { addItems: (state.addItems = event.target.value) }
         })
     }
 
     adder = () => {
         this.setState((state) => {
-            return { item: state.item.concat(this.state.addItems),
-                addItems: '' 
-            }
+            return { item: state.item.concat(this.state.addItems), addItems: ''} 
         })
     }
+
+    remover = (item) => {
+        this.setState((state) => {
+            const array = state.item.filter((todo) => {
+                return todo !== item;
+            });
+            return {item: (state.item = array) };
+        });
+    }
+
 
     reset = () => {this.setState({item:[
         'Latte', 'Pane', 'Carne', 'Pesce'
     ]})}
 
     render() {
+
         return (
             <div>
-                <ul>{this.state.item.map((item, index) => (<li key={index}>{item}</li>))}</ul>
-                <input type="text" value={this.state.addItems} onChange={this.eventHandler} />
-                <button onClick={this.adder}>Add</button>
-                <button onClick={this.reset}>Reset</button>
-            </div>
-        )
-    }
+        <ul>
+          {this.state.item.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button
+                key={index + "btn"}
+                onClick={() => {
+                  this.remover(item);
+                }}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={this.state.addItems}
+          onChange={this.eventHandler}
+        />
+        <button onClick={this.adder}>Aggiungi</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
 }
 
