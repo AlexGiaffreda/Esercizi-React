5b3648dd9db4f26c4823572f3dd5583df1a609d8
Alex <alex.giaffreda7@gmail.com>
2023-02-06 17:15:32 +0100

    lists 3

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default  class Todolist extends React.Component {
+
+    state = {
+        item: [
+            'Latte', 'Pane', 'Carne', 'Pesce'
+        ],
+        addItems: ''
+    }
+
+    eventHandler = (event) => {
+        this.setState((state) => {
+            return { addItems: state.addItems = event.target.value }
+        })
+    }
+
+    eventSaver = () => {
+        this.setState((state) => {
+            return { item: state.item.concat(this.state.addItems) }
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <ul>{this.state.item.map((item, index) => (<li key={index}>{item}</li>))}</ul>
+                <input type="text" value={this.state.addItems} onChange={this.eventHandler} />
+                <button onClick={this.eventSaver}>Aggiungi</button>
+            </div>
+        )
+    }
+}
+
