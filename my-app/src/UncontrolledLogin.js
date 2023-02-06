d4043f6d4a8201d24580d368df7f1985308e8dd3
Alex <alex.giaffreda7@gmail.com>
2023-02-06 16:47:39 +0100

    forms 5

---
+++ my-app/src/UncontrolledLogin.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default class UncontrolledLogin extends React.Component{
+
+    eventHandler = (event) => {
+        const username = event.target.elements.username.value
+        const password = event.target.elements.password.value
+        const check = event.target.elements.check.checked
+
+        console.log(`${username}, ${password}, ${check}`)
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.eventHandler}>
+                    <input type='username' name='username'></input>
+                    <input type='password' name='password'></input>
+                    <input type='checkbox' name='check'></input>
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
+        )
+    }
+}
