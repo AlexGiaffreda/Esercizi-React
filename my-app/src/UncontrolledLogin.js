24d247dbf747922e3b6912cfad925f806eae1b0c
Alex <alex.giaffreda7@gmail.com>
2023-02-06 16:49:14 +0100

    forms 6

--- my-app/src/UncontrolledLogin.js
+++ my-app/src/UncontrolledLogin.js
@@ -14,7 +14,7 @@ export default class UncontrolledLogin extends React.Component{
         return(
             <div>
                 <form onSubmit={this.eventHandler}>
-                    <input type='username' name='username'></input>
+                    <input type='username' name='username' autoFocus></input>
                     <input type='password' name='password'></input>
                     <input type='checkbox' name='check'></input>
                     <button type="submit">Log in</button>
