2cbfc3adc80e51141c82b3391aad082ab1a70483
Alex <alex.giaffreda7@gmail.com>
2023-02-11 16:41:18 +0100

    stulying 1

---
+++ my-app/src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Welcome extends React.Component{
+    render(){
+        return(
+            <div className='welcome'>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
