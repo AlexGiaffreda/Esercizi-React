29537abceb5751e96152139506ee3189332c90ea
Alex <alex.giaffreda7@gmail.com>
2023-02-11 18:27:22 +0100

    func-comp 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import Welcome from "./Welcome";
+import Sum from "./Sum";
 
 export default function App() {
   return (
     <div className="App">
       <Welcome />
+      <Sum numbers={[4, 1, 41]} />
     </div>
   )
 }
---
+++ my-app/src/Sum.js
@@ -0,0 +1,7 @@
+export default function Sum(props){
+    return(
+        <h1>
+            {props.numbers.reduce((total, current) => {return (total += current) })}
+        </h1>
+    )
+}
