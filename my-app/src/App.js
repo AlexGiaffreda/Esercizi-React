68e537261bfb01f3ee6e82044098ae673e3b2480
Alex <alex.giaffreda7@gmail.com>
2023-02-12 19:02:27 +0100

    effect 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,47 @@ import Counter from './Counter';
 import Login from './Login';
 import { useState } from 'react';
 import GithubUser from './GithubUser';
+const array = ["AlexGiaffreda", "CcCcCc99"];
 
 function App() {
 
-  const [toggle, setToggle]= useState(true)
 
-  function onCounterChange(counter){
-    console.log(`counter: ${counter}`)
+  const [toggle, setToggle] = useState(true)
+  const [name, setName] = useState("")
+
+  function insertNameHandler(event) {
+    setName(event.target.value);
+  }
+  
+  function launchData(event) {
+    event.preventDefault();
+    array.push(event.target.previousElementSibling.defaultValue);
+    console.log(array);
+    setName("");
   }
 
-  function handleButton(){
-    setToggle((s) => !s)
+  function onCounterChange(counter) {
+    console.log(`The count now is ${counter}`);
+  }
+
+  function handleButton() {
+    setToggle((s) => !s);
+ 
   }
   return (
     <div className="App">
       <Login />
       {toggle && <Counter onCounterChange={onCounterChange}/>}
       <button onClick={handleButton}>Toggle</button>
-      <GithubUser username={'AlexGiaffreda'}/>
+      {array.map ((element)=>{return(
+        <div>
+        <GithubUser username={element}/>
+        </div>
+        )})}
+     <form onSubmit={launchData}>
+     <input type='text' name='insertName' value={name} onChange={insertNameHandler} ></input>
+     <button type='submit' onClick={launchData}>Start</button>
+     </form>
     </div>
     
   );
