817959ff2c44ae07060ad5d8a6b3ab59abbea76e
Alex <alex.giaffreda7@gmail.com>
2023-02-12 17:42:01 +0100

    router 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
 import Counter from './Counter';
-
+import ShowGithubUser from './ShowGithubUser';
 
 function App() {
   return (
@@ -9,6 +9,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Welcome name='Alex'/>}></Route>
         <Route path='/counter' element={<Counter/>}></Route>
+        <Route path='/githubuser'>
+          <Route path=':name' element={<ShowGithubUser />} />
+        </Route>
       </Routes>
     </div>
   );
