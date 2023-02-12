2c50615f74dd1363be5b6b15146b200f4aa146fc
Alex <alex.giaffreda7@gmail.com>
2023-02-12 17:36:19 +0100

    router 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
+import Counter from './Counter';
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Welcome name='Alex'/>}></Route>
+        <Route path='/counter' element={<Counter/>}></Route>
       </Routes>
     </div>
   );
